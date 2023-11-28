55277c6120792445111136a9d3ed6abda5f9acd5
crapthings <crapthings@gmail.com>
2023-11-28 23:59:17 +0800

    rebuild

--- cache.py
+++ cache.py
@@ -1,15 +1,18 @@
 import torch
-from diffusers import StableDiffusionPipeline
+from diffusers import StableDiffusionInpaintPipeline
+from compel import Compel
 
 from config import model_name
 
 print('cache model')
 
-StableDiffusionPipeline.from_single_file(
+pipe = StableDiffusionInpaintPipeline.from_single_file(
   model_name,
   torch_dtype = torch.float16,
   variant = 'fp16',
   use_safetensors = True
 )
+
+compel = Compel(tokenizer = pipe.tokenizer, text_encoder = pipe.text_encoder)
 
 print('done')
